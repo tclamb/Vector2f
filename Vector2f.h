6e188ac02066c81a4bebaaeec858091dcf4e4b7f
tclamb <tclamb@ufl.edu>
2013-06-03 16:59:41 -0400

    Move Coord to its own header

--- Vector2f.h
+++ Vector2f.h
@@ -1,14 +1,5 @@
-#ifndef _Vector2f_h_
-#define _Vector2f_h_
-
-struct Coord {
-    int x;
-    int y;
-
-    Coord() : x(0), y(0) {};
-    Coord(int const& _x, int const& _y) : x(_x), y(_y) {};
-    Coord(Coord const& c) : x(c.x), y(c.y) {};
-};
+#ifndef _Vector2f_Vector2f_h_
+#define _Vector2f_Vector2f_h_
 
 struct Vector2f {
     float x;
